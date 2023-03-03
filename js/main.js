f87e89fbc066323d7448c911944d377b7756bc51
lolo <laurasw86@gmail.com>
2023-03-02 22:20:07 -0500

    Added floating jump-to-top button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const button = document.querySelector("button");
-const body = document.querySelector("body");
-const filterButton = document.querySelector(".filters");
+"use strict";
 
-button.addEventListener("click", function () {
-  if (body.classList.contains("dark-mode")) {
-    body.classList.remove("dark-mode");
+const darkModeButton = document.querySelector(".dark-mode");
+const body = document.querySelector("body");
+const backToTopButton = document.querySelector(".home-button");
+const backToTopAnchor = document.querySelector("#jump-to-top");
+
+darkModeButton.addEventListener("click", function () {
+  if (body.classList.contains("dark-mode-theme")) {
+    body.classList.remove("dark-mode-theme");
+    darkModeButton.innerText = "Dark Mode";
   } else {
-    body.classList.add("dark-mode");
+    body.classList.add("dark-mode-theme");
+    darkModeButton.innerText = "Light Mode";
   }
+});
+
+backToTopButton.addEventListener("click", function () {
+  backToTopAnchor.onclick = () => window.scrollTo({ top: 0 });
 });
