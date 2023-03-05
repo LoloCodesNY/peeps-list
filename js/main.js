7489a8977ca32b201ad1be4a3b9ac5293f031ca4
lolo <laurasw86@gmail.com>
2023-03-05 14:50:19 -0500

    Added dark mode functionality and themes

--- js/main.js
+++ js/main.js
@@ -3,18 +3,97 @@
 const darkModeButton = document.querySelector(".dark-mode");
 const body = document.querySelector("body");
 const backToTopButton = document.querySelector(".home-button");
-const backToTopAnchor = document.querySelector("#jump-to-top");
+const container = document.querySelector(".container");
+const peepsList = document.querySelector("ol");
+const sortByHoliday = document.querySelector(".select");
+const holidayOption = document.querySelector(".filter-option");
+const peepListItems = document.querySelectorAll(".peep");
+let selection = "All Flavors";
+
+//Dark mode
 
 darkModeButton.addEventListener("click", function () {
   if (body.classList.contains("dark-mode-theme")) {
     body.classList.remove("dark-mode-theme");
     darkModeButton.innerText = "Dark Mode";
+    darkModeButton.classList.add("dark-mode");
   } else {
     body.classList.add("dark-mode-theme");
+    darkModeButton.classList.add("light-mode");
     darkModeButton.innerText = "Light Mode";
   }
 });
 
-backToTopButton.addEventListener("click", function () {
-  backToTopAnchor.onclick = () => window.scrollTo({ top: 0 });
-});
+//Scroll to top button
+
+backToTopButton.onclick = () => window.scrollTo({ top: 0 });
+
+//Infinite scroll and load more button
+
+//Sort by holiday: Halloween, Christmas, Easter
+
+// sortByHoliday.addEventListener("change", function (e) {
+//   selection = e.target.value;
+//   if (selection === "Halloween") {
+//     if (!peepListItems.classList.contains("Halloween")) {
+//     }
+//     peepListItems.classList.add("hide");
+//   }
+// });
+
+// sortByHoliday.addEventListener("change", function (e) {
+//   selection = e.target.value;
+//   if (selection === "Halloween") {
+//     for (let peep of peepListItems) {
+//       if (!peep.classList.contains("halloween")) {
+//         peep.classList.add("hide");
+//       }
+//     }
+//   }
+// });
+
+// sortByHoliday.addEventListener("change", function (e) {
+//   selection = e.target.value;
+//   if (selection === "Christmas") {
+//     for (let peep of peepListItems) {
+//       if (peep.classList.contains("hide")) {
+//         peep.classList.remove("hide");
+//       }
+//       if (!peep.classList.contains("christmas")) {
+//         peep.classList.add("hide");
+//       }
+//     }
+//   }
+// });
+
+// sortByHoliday.addEventListener("change", function (e) {
+//   selection = e.target.value;
+//   if (selection === "Easter") {
+//     for (let peep of peepListItems) {
+//       if (!peep.classList.contains("easter")) {
+//         peep.classList.add("hide");
+//       }
+//     }
+//   }
+// });
+
+// const allFlavorsSort = sortByHoliday.addEventListener("change", function (e) {
+//   selection = e.target.value;
+//   if (selection === "All Flavors") {
+//     for (let peep of peepListItems) {
+//       peep.classList.remove("hide");
+//     }
+//   }
+// });
+
+// sortByHoliday.addEventListener("change", function (e) {
+//   selection = e.target.value;
+//   if (selection === "Halloween") {
+//     for (let peep of peepListItems) {
+//       if (peep.classList.contains("halloween")) {
+//         console.log(peep);
+//         return peep;
+//       }
+//     }
+//   }
+// });
